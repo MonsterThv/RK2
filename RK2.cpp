31a175bcfb017d97aa87a9fa8502c115eb3f2b7a
Sergey <s9262406004@gmail.com>
2024-11-30 11:33:44 +0300

    Create RK2.cpp

---
+++ RK2.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void insert(int*& arr, int pos, int el, int& len) {
+	if (len + 1 >= pos && pos > 0) {
+		int* temp = new int[len + 1];
+		bool flag = false;
+		for (int i = 0; i < len + 1; i++) {
+			if (!flag && i != pos) {
+				temp[i] = arr[i];
+			}
+			if (flag) {
+				temp[i] = arr[i - 1];
+			}
+			if (pos - 1 == i) {
+				flag = true;
+				temp[i] = el;
+			}
+
+		}
+		len++;
+		arr = temp;
+	}
+	else {
+		cout << "wrong pos" << endl;
+	}
+}
+
+int main()
+{
+	int len = 3;
+	int* arr = new int[len];
+	arr[0] = 1;
+	arr[1] = 2;
+	arr[2] = 3;
+	insert(arr, 2, 4, len);
+	for (int i = 0; i < len; i++) {
+		cout << arr[i] << ' ';
+	}
+}
+
